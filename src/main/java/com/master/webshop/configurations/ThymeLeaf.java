4a8e0b997abfc51c38a29c089ddd84fa432750b3
Marcin Dawidowski <marcin.dawidowskipl@gmail.com>
2019-07-27 13:12:42 +0200

    home page

---
+++ src/main/java/com/master/webshop/configurations/ThymeLeaf.java
@@ -0,0 +1,13 @@
+package com.master.webshop.configurations;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ThymeLeaf {
+    @Bean
+    public LayoutDialect layoutDialect(){
+        return new LayoutDialect();
+    }
+}
--- src/main/java/com/master/webshop/controller/HomeController.java
+++ src/main/java/com/master/webshop/controller/HomeController.java
@@ -13,6 +13,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String getHome(Model theModel){
-        return "home";
+        return "index";
     }
 }
